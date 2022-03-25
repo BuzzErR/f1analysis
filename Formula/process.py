391ff17298cedc08653597f1096ba46611ccff95
BuzzErR <new.mikhailforwork@gmail.com>
2022-03-25 23:11:02 +0300

    new data added

--- Formula/process.py
+++ Formula/process.py
@@ -1,7 +1,6 @@
 import datetime
 import logging
-from multiprocessing import Process, current_process, Queue
-import multiprocessing
+from multiprocessing import Process, current_process
 from importlib import reload
 import numpy as np
 import pandas as pd
@@ -24,22 +23,46 @@ def get_logger():
     return log
 
 
-def get_driver_ahead_speed(data, driver_row, pbar) -> np.int_:
-    driver_ahead_num = driver_row['DriverAhead']
-    pbar.update(1)
-    if str(driver_ahead_num) == '':
-        return np.NaN
-    driver_ahead_data = data[data['DriverNumber'] == driver_ahead_num]
-    driver_ahead_data = driver_ahead_data[(driver_ahead_data['Time'] >= driver_row['Time']) &
-                                          (driver_ahead_data['Time'] < driver_row['Time'] + pd.Timedelta("1s")) &
-                                          (driver_ahead_data['X_sector'] == driver_ahead_data['X_sector']) &
-                                          (driver_ahead_data['Y_sector'] == driver_ahead_data['Y_sector'])]
-    if len(driver_ahead_data) == 0:
-        return np.NaN
-    return driver_ahead_data.iloc[0]['Speed']
+def get_driver_ahead_data(data, driver_data):
+    driver_ahead_data_to_return = {"Driver_ahead_speed": [], "Driver_ahead_DRS": [], "Driver_ahead_throttle": [],
+                                   "Overtake": []}
+    columns_of_ahead = ["Driver_ahead_speed", "Driver_ahead_DRS", "Driver_ahead_throttle", "Overtake"]
+    driver_ahead_data_to_return = pd.DataFrame(driver_ahead_data_to_return)
+    driver_data = driver_data.reset_index(drop=True)
+    current = current_process()
+    for index, driver_row in tqdm(driver_data.iloc[:-1].iterrows(), total=driver_data.shape[0] - 1,
+                                  position=current._identity[0] - 1, desc=f'Driver: {current.name}'):
+        driver_ahead_num = driver_row['DriverAhead']
+        driver_ahead_data = data[data['DriverNumber'] == driver_ahead_num]
+        ahead_row = driver_ahead_data[(driver_ahead_data['Time'] >= driver_row['Time']) &
+                                      (driver_ahead_data['Time'] < driver_row['Time'] + pd.Timedelta("0.5s")) &
+                                      (driver_ahead_data['X_sector'] == driver_row['X_sector']) &
+                                      (driver_ahead_data['Y_sector'] == driver_row['Y_sector'])].sort_values(
+            by=['Time'])
+        if str(driver_ahead_num) == '' or len(ahead_row) == 0:
+            data_to_append = np.empty(4)
+            data_to_append[:] = np.nan
+            data_to_append[3] = 0
+        else:
+            ahead_driver_row = ahead_row.iloc[0][['Speed', 'DRS', 'Throttle']].to_list()
+            overtake_completed = 0
+            next_row = driver_data.iloc[index + 1]
+            if next_row['DriverAhead'] != driver_ahead_num:
+                next_sector_tick_driver_ahead = driver_ahead_data[
+                    (driver_ahead_data['Time'] >= next_row['Time'] - pd.Timedelta("0.5s")) &
+                    (driver_ahead_data['Time'] < next_row['Time'] + pd.Timedelta("0.5s")) &
+                    (driver_ahead_data['X_sector'] == next_row['X_sector']) &
+                    (driver_ahead_data['Y_sector'] == next_row['Y_sector'])].sort_values(by=['Time'])
+                if len(next_sector_tick_driver_ahead) > 0 and \
+                        next_sector_tick_driver_ahead.iloc[0]['Time'] > next_row['Time']:
+                    overtake_completed = 1
+            ahead_driver_row.append(overtake_completed)
+            data_to_append = ahead_driver_row
+        driver_ahead_data_to_return.append(pd.DataFrame([data_to_append], columns=columns_of_ahead))
+    return driver_ahead_data_to_return
 
 
-def process_driver(data, driver_num, lock):
+def process_driver(data, driver_num):
     log = get_logger()
     driver_data = data[data['DriverNumber'] == driver_num].copy()
     driver_data['X_sector_diff'] = driver_data['X_sector'].diff()
@@ -47,16 +70,9 @@ def process_driver(data, driver_num, lock):
     driver_data = driver_data[(driver_data['X_sector_diff'] != 0) | (driver_data['Y_sector_diff'] != 0)]
     start_time = datetime.datetime.now()
     log.info(f'started processing for {driver_num}')
-    tqdm.pandas()
     current = current_process()
-    with lock:
-        bar = tqdm(
-            desc=f'Driver: {current.name}',
-            total=len(driver_data),
-            position=current._identity[0] - 1,
-            leave=False
-        )
-    driver_data['Ahead_driver_speed'] = driver_data.apply(lambda d: get_driver_ahead_speed(data, d, bar), axis=1)
+    driver_ahead_data = get_driver_ahead_data(data, driver_data)
+    driver_data = pd.concat([driver_data, driver_ahead_data], axis=1)
     log.info(f'processing by: {current.name} ended, took {datetime.datetime.now() - start_time}')
     driver_data.drop(columns=['X_sector_diff', 'Y_sector_diff'], inplace=True)
     driver_data.to_csv(f"{current.name}.csv")
@@ -64,6 +80,8 @@ def process_driver(data, driver_num, lock):
 
 
 def form_overall_df(data, drivers_list, x_sector_length, y_sector_length) -> pd.DataFrame:
+    if len(drivers_list) == 0:
+        return pd.DataFrame({})
     return_data = data.pick_driver(drivers_list[0]).get_telemetry()
     return_data['DriverNumber'] = [drivers_list[0]] * len(return_data)
     for driver_to_process in drivers_list[1:]:
@@ -105,11 +123,10 @@ def get_drivers_to_process(drivers_list, log):
 
 if __name__ == '__main__':
 
-    X_SIZE_OF_SECTOR = 50
-    Y_SIZE_OF_SECTOR = 50
+    X_SIZE_OF_SECTOR = 100
+    Y_SIZE_OF_SECTOR = 100
     NUM_OF_THREADS = 4
 
-    lock_mp = multiprocessing.Manager().Lock()
     ff1.Cache.enable_cache('cache')
     session = ff1.get_session(2021, 20, 'R')
     laps = session.load_laps(with_telemetry=True)
@@ -118,10 +135,7 @@ if __name__ == '__main__':
 
     drivers, preloaded_data = get_drivers_to_process(list(laps['DriverNumber'].unique()), logger)
     logger.info(f'{len(drivers)} to process')
-    all_drivers_data = pd.DataFrame({})
-    if len(drivers) > 0:
-        all_drivers_data = form_overall_df(laps, drivers, X_SIZE_OF_SECTOR, Y_SIZE_OF_SECTOR)
-
+    all_drivers_data = form_overall_df(laps, drivers, X_SIZE_OF_SECTOR, Y_SIZE_OF_SECTOR)
     planed_threads = list()
     active_threads = list()
 
@@ -129,7 +143,7 @@ if __name__ == '__main__':
         thread = Process(
             target=process_driver,
             name=driver,
-            args=(all_drivers_data.copy(), driver, logger, lock_mp))
+            args=(all_drivers_data.copy(), driver))
         planed_threads.append(thread)
 
     i = 0
